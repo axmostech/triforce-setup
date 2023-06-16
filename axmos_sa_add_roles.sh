41703237bd3da42ae3868ff38bdfbc208d0fd2ba
Felipe Velasquez <felipe.velasquezc@gmail.com>
2023-06-16 11:42:10 -0400

    Separate project permissions from org permissions

--- axmos_sa_add_roles.sh
+++ axmos_sa_add_roles.sh
@@ -12,18 +12,15 @@ echo "Org Id: "$ORG_ID;
 AXMOS_SA_ROLES=(
 'roles/appengine.appViewer' \
 'roles/artifactregistry.reader' \
-'roles/bigquery.dataEditor' \
 'roles/bigquery.metadataViewer' \
 'roles/bigtable.viewer' \
 'roles/billing.viewer' \
-'roles/cloudasset.owner' \
 'roles/cloudfunctions.viewer' \
 'roles/cloudsql.viewer' \
 'roles/composer.user' \
 'roles/compute.networkViewer' \
 'roles/compute.viewer' \
 'roles/container.viewer' \
-'roles/containerregistry.ServiceAgent' \
 'roles/dataflow.viewer' \
 'roles/dataproc.viewer' \
 'roles/datastore.viewer' \
@@ -41,30 +38,50 @@ AXMOS_SA_ROLES=(
 'roles/resourcemanager.organizationViewer' \
 'roles/run.viewer' \
 'roles/secretmanager.viewer' \
-'roles/serviceusage.serviceUsageAdmin' \
-'roles/storage.admin' \
 'roles/recommender.viewer' \
 'roles/securitycenter.adminEditor' \
-'roles/cloudbuild.builds.editor' \
 'roles/serviceusage.serviceUsageConsumer' \
-'roles/run.admin' \
 'roles/iam.serviceAccountUser' \
 'roles/artifactregistry.writer' \
-'roles/monitoring.viewer' \
-'roles/bigquery.jobUser' \
-'roles/bigquery.dataOwner' \
-'roles/bigquery.user' 
+'roles/monitoring.viewer' \ 
+'roles/bigquery.metadataViewer' \
+'roles/serviceusage.serviceUsageAdmin' \
+'roles/containerregistry.ServiceAgent' \
+'roles/cloudasset.owner' 
 )
 
 for role in "${AXMOS_SA_ROLES[@]}"
 do
-  echo "Assigning $role... to $SA_NAME";
+  echo "Assigning $role... to $SA_NAME at the organization";
   gcloud organizations add-iam-policy-binding ${ORG_ID} \
     --member=serviceAccount:"$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
     --role="$role" \
     --no-user-output-enabled --quiet;
 done
 
+ON_PROJECT_PERMISIONS=(
+  'roles/run.admin' \
+  'roles/cloudbuild.builds.editor' \
+  'roles/storage.admin' \
+  'roles/bigquery.jobUser' \
+  'roles/bigquery.dataOwner' \
+  'roles/bigquery.user'
+)
+
+for role in "${ON_PROJECT_PERMISIONS[@]}"
+do
+  echo "Assigning $role... to $SA_NAME int the project";
+
+  gcloud iam service-accounts add-iam-policy-binding $SA_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+    --member=serviceAccount:"$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="$role" \
+    --no-user-output-enabled --quiet;
+done
+
+
+echo "Trying to download service account key .json";
 gcloud iam service-accounts keys create ~/sa.json \
   --iam-account "$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com"
+
+echo "If it fails please download it manually in the IAM secction of $PROJECT_ID project.";
 
